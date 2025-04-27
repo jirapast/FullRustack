5d24d59d6efa639441c044f2c87c0fffec406042
jiissee <jiissee@gmail.com>
2025-04-27 22:03:22 +0700

    simple CRUD using sea_orm

--- ProjectSmartShop/src/main.rs
+++ ProjectSmartShop/src/main.rs
@@ -1,73 +1,105 @@
-// use actix_web::web::block;
+use actix_web::web::block;
 use futures::executor::block_on;
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{ActiveValue, Database, DatabaseConnection};
 use sea_orm::{ConnectionTrait, DbBackend, DbErr, Statement};
+use sea_orm::*;
+use std::env;
+use crate::entities::demo_items::Model; // Add this import
+
+mod entities;
+use entities::{prelude::*, *};
+
+const DATABASE_URL: &str = "sqlite://db/mydb.sqlite";
+const DB_NAME: &str = "tmp_table";
 
 
-const DATABASE_URL: &str = "sqlite://db/mydb.sqlite?mode=ro";
-const DB_NAME = "tmp_table";
+async fn connect_db() -> DatabaseConnection {
+    let db = Database::connect(DATABASE_URL)
+        .await
+        .expect("Database connection failed");
+    db
+}
 
-async fn run() -> Result<(), DbErr> {
-    let db: DatabaseConnection = Database::connect(DATABASE_URL).await?;
-    println!("Database connected successfully!");
+async fn find_row(id: i32, db: &DatabaseConnection) -> Option<Model> {
+    let r= DemoItems::find_by_id(id)
+        .one(db)
+        .await
+        .unwrap();
+    r 
+}
 
-    // 
-    let db = &match db.get_database_backend() {
-        DbBackend::MySql => {
-+           db.execute(Statement::from_string(
-+               db.get_database_backend(),
-+               format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
-+           ))
-+           .await?;
-+
-+           let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-+           Database::connect(&url).await?
-+       }
-
-        DbBackend::Sqlite => {
-            db.execute(Statement::from_string(
-                db.get_database_backend(),
-                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
-            )).await?;
+async fn insert_row(row_id: i32, db: &DatabaseConnection) -> Result<(), DbErr> {
+    let row = find_row(row_id, &db).await;
+    match row {
+        Some(_) => {
+            println!("the key already exists; cannot insert into key {}", row_id) ;
         }
-        let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-        Database::connect(&url).await?
-
-// +       DbBackend::Postgres => {
-// +           db.execute(Statement::from_string(
-// +               db.get_database_backend(),
-// +               format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
-// +           ))
-// +           .await?;
-// +           db.execute(Statement::from_string(
-// +               db.get_database_backend(),
-// +               format!("CREATE DATABASE \"{}\";", DB_NAME),
-// +           ))
-// +           .await?;
-// +
-// +           let url = format!("{}/{}", DATABASE_URL, DB_NAME);
-// +           Database::connect(&url).await?
-
-        // 
-
-+       }
-        BDBackedend::Sqlite => db
-    };
-
-    // 
-
+        None => {
+            let to_update= demo_items::ActiveModel {
+                id: Set(row_id),
+                name: Set("val".to_owned()),
+            };
+            println!("the key is not exists; can insert into key {:?}", to_update) ;
+            to_update.insert(db).await?;
+            println!("Successfully inserted key");
+        }
+    }
     Ok(())
 }
 
-// #[derive(Iden)]
-enum Item {
-    id,
-    name,
+async fn delete_row(row_id: i32, db: &DatabaseConnection) -> Result<(), DbErr> {
+    let row = find_row(row_id, &db).await;
+    match row {
+        Some(_) => {
+            println!("the key already exists; can delete into key {}", row_id) ;
+            let to_update= demo_items::ActiveModel {
+                id: Set(row_id),
+                name: Set("val".to_owned()),
+            };
+            to_update.delete(db).await?;
+            println!("Successfully deleted key");
+        }
+        None => {
+            println!("the key is not exists; cannot delete into key {:?}", row_id) ;
+        }
+    }
+    Ok(())
+}
+
+async fn update_row(row_id: i32, val: &str, db: &DatabaseConnection) -> Result<(), DbErr> {
+    let row = find_row(row_id, &db).await;
+    match row {
+        Some(_) => {
+            println!("the key already exists; canupdate into key {}", row_id) ;
+            let to_update= demo_items::ActiveModel {
+                id: Set(row_id),
+                name: Set(val.to_owned()),
+            };
+            to_update.update(db).await?;
+            println!("Successfully updateed key");
+        }
+        None => {
+            println!("the key is not exists; cannot update into key {:?}", row_id) ;
+        }
+    }
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    if let Err(err) = block_on(run()) {
-        panic!("{}", err);
-    }
+    // connect
+    let db = connect_db().await;
+        
+    // select
+    let row = find_row(5, &db).await;
+    println!("row {:?}", row);
+
+    // insert
+    insert_row(5, &db).await;
+
+    // delete
+    delete_row(5, &db).await;
+
+    // update
+    update_row(3, "<><><>", &db).await;
 }
